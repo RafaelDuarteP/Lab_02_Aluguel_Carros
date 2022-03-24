7fcad684755a76a18416320c57e7d66749cd2cb9
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2022-03-23 21:27:12 -0300

    Arquivos para novo usuario

--- desenvolvimento/src/main/java/com/example/demo/cotroller/UsuarioController.java
+++ desenvolvimento/src/main/java/com/example/demo/cotroller/UsuarioController.java
@@ -1,13 +1,20 @@
 package com.example.demo.cotroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.dao.UsuarioDao;
 
 @Controller
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioDao usuarioDao;
+
 	@GetMapping("/usuario/new")
 	public String newUsuario() {
+
 		return "usuarios/new";
 	}
 }
