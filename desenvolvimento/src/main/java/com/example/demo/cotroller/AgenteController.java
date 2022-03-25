26641f947254dd9eb9ea83d7a639c11bef833476
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2022-03-24 22:43:39 -0300

    controller de agente

---
+++ desenvolvimento/src/main/java/com/example/demo/cotroller/AgenteController.java
@@ -0,0 +1,92 @@
+package com.example.demo.cotroller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.dao.UsuarioDao;
+import com.example.demo.model.users.Agente;
+import com.example.demo.model.users.Endereco;
+import com.example.demo.model.users.Usuario;
+
+@Controller
+public class AgenteController {
+	@Autowired
+	UsuarioDao usuarioDao;
+
+	@PostMapping("/agente/new")
+	public String novoAgente(Agente a, Endereco e) {
+		a.setEndereco(e);
+		usuarioDao.save(a);
+		return "redirect:/usuario";
+	}
+
+	@GetMapping("/agente/{id}")
+	public ModelAndView show(@PathVariable long id) {
+		Optional<Usuario> user = usuarioDao.findById(id);
+		ModelAndView mv;
+
+		if (user.isPresent()) {
+			mv = new ModelAndView("agente/show");
+			try {
+				Agente c = (Agente) user.get();
+				mv.addObject("a", c);
+			} catch (ClassCastException e) {
+				mv = new ModelAndView("redirect:/usuario");
+			}
+
+		} else {
+			mv = new ModelAndView("redirect:/usuario");
+		}
+		return mv;
+
+	}
+	
+	@GetMapping("/agente/{id}/edit")
+	public ModelAndView edit(@PathVariable long id) {
+		Optional<Usuario> user = usuarioDao.findById(id);
+		ModelAndView mv;
+
+		if (user.isPresent()) {
+			mv = new ModelAndView("agente/edit");
+			try {
+				Agente c = (Agente) user.get();
+				mv.addObject("c", c);
+			} catch (ClassCastException e) {
+				mv = new ModelAndView("redirect:/usuario");
+			}
+
+		} else {
+			mv = new ModelAndView("redirect:/usuario");
+		}
+		return mv;
+
+	}
+	
+	@PostMapping("/agente/{id}")
+	public ModelAndView update(@PathVariable long id, Agente agente, Endereco end) {
+		Optional<Usuario> user = usuarioDao.findById(id);
+		ModelAndView mv;
+
+		if (user.isPresent()) {
+			mv = new ModelAndView("redirect:/agente/" + id);
+			try {
+				Agente c = (Agente) user.get();
+				c.update(agente);
+				usuarioDao.save(c);
+			} catch (ClassCastException e) {
+				mv = new ModelAndView("redirect:/usuario");
+			}
+
+		} else {
+			mv = new ModelAndView("redirect:/usuario");
+		}
+		return mv;
+	}
+
+}
